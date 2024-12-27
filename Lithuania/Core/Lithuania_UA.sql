ff0a5b62422d7d366bed34b4f8848a8874c76ea3
Benjamin Andula <andula.benjamin@bbox.fr>
2024-12-27 21:04:15 +0100

    Appeal on features on UA + Clean of Faith Bursts

--- Lithuania/Core/Lithuania_UA.sql
+++ Lithuania/Core/Lithuania_UA.sql
@@ -48,29 +48,49 @@ FROM Districts WHERE RequiresPlacement = 1 AND Coast = 1
 
 -----------------------------------------------
 -- -50%[ICON_Faith]Faith per turn. 
--- You gain bursts of 50 [ICON_Faith] Faith when finishing [ICON_District] Districts on Breathtaking tile or better. 
--- Founding a [ICON_Religion] religion halves the Faith gain through built District and grant +1 [ICON_Governor] Governor Title.
+-- You gain bursts of Faith (scaling with era) when finishing Districts on Breathtaking tiles, doubled if built onto Floodplains or Marshes.
 -----------------------------------------------
 
+--- Preparation of tables to ease next things
+
+CREATE TABLE IF NOT EXISTS Rwb_FaithReference_UA
+(
+    EraType TEXT,
+    Value INT
+);
+
+CREATE TABLE IF NOT EXISTS TempFaithTable
+(
+    Value INT
+);
+
+WITH RECURSIVE t(val) AS (SELECT 13 UNION ALL SELECT val + 4 + val/3 FROM t LIMIT (SELECT COUNT(*) FROM Eras))
+INSERT OR REPLACE INTO TempFaithTable (Value) SELECT val FROM t;
+
+INSERT OR REPLACE INTO Rwb_FaithReference_UA
+(EraType,Value)
+SELECT
+    e.EraType,
+    f.Value
+FROM TempFaithTable f, Eras e
+WHERE f.rowid = e.rowid;
+
+DROP TABLE TempFaithTable;
+
+------------------------
+
 INSERT OR REPLACE INTO Types
-            (Type, Kind) 
+            (Type,                                        Kind) 
 VALUES      ('MODIFIER_PLAYER_DISTRICTS_ATTACH_MODIFIER', 'KIND_MODIFIER');
 
 INSERT OR REPLACE INTO DynamicModifiers
-            (ModifierType, CollectionType, EffectType) 
+            (ModifierType,                                  CollectionType,                 EffectType) 
 VALUES      ('MODIFIER_PLAYER_DISTRICTS_ATTACH_MODIFIER',   'COLLECTION_PLAYER_DISTRICTS',  'EFFECT_ATTACH_MODIFIER'); 
 
 
 INSERT OR REPLACE INTO	TraitModifiers
-            (TraitType,									                    ModifierId								)
-VALUES      
-            ('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_DISTRICTS_FAITH_MODIFIER_GIVER'),
-            ('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_DISTRICTS_FAITH_MODIFIER_GIVER_RELIGION_FOUNDED'),
-
-            /*('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_PLAYER_MODIFIER_GIVER'),
-            ('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_PLAYER_MODIFIER_GIVER_GIVER_RELIGION_FOUNDED'),*/
-
-            ('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_GIVE_GOVERNOR_TITLE'),
+            (TraitType,									    ModifierId								)
+VALUES      ('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_DISTRICTS_FAITH_MODIFIER_GIVER'),
 
             ('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_FAITH_GENERATION_MALUS');
 
@@ -86,30 +106,13 @@ INSERT OR REPLACE INTO Modifiers
 VALUES      ('RWB_DIEVDIRBIAI_DISTRICTS_FAITH_MODIFIER_GIVER',
              'MODIFIER_PLAYER_DISTRICTS_ATTACH_MODIFIER',
              'REQSET_RWB_DIEVDIRBIAI_HAS_BREATHTAKING_TILE',
-             'REQSET_RWB_DIEVDIRBIAI_GREATPROPHET_EXISTS',
+             null,
              0),
-            ('RWB_DIEVDIRBIAI_GREATPROPHET_FAITH_GAIN',
-             'MODIFIER_PLAYER_GRANT_YIELD',
-             null,
-             null,
-             1),
     
     
-            ('RWB_DIEVDIRBIAI_DISTRICTS_FAITH_MODIFIER_GIVER_RELIGION_FOUNDED',
-             'MODIFIER_PLAYER_DISTRICTS_ATTACH_MODIFIER',
-             'REQSET_RWB_DIEVDIRBIAI_HAS_BREATHTAKING_TILE',
-             'REQSET_RWB_DIEVDIRBIAI_FOUNDED_RELIGION_ONCE',
-             0),
-            ('RWB_DIEVDIRBIAI_RELIGION_FAITH_GAIN',
+            ('RWB_DIEVDIRBIAI_FAITH_BURSTS',
              'MODIFIER_PLAYER_GRANT_YIELD',
              null,
-             null,
-             1),
-
-
-            ('RWB_DIEVDIRBIAI_GIVE_GOVERNOR_TITLE',
-             'MODIFIER_PLAYER_ADJUST_GOVERNOR_POINTS',
-             'REQSET_RWB_DIEVDIRBIAI_FOUNDED_RELIGION_ONCE',
              null,
              1),
 
@@ -123,107 +126,78 @@ VALUES      ('RWB_DIEVDIRBIAI_DISTRICTS_FAITH_MODIFIER_GIVER',
 
 INSERT OR REPLACE INTO ModifierArguments
             (ModifierId,                                                                                Name,                   Value)
-VALUES      ('RWB_DIEVDIRBIAI_DISTRICTS_FAITH_MODIFIER_GIVER',                      'ModifierId',        'RWB_DIEVDIRBIAI_GREATPROPHET_FAITH_GAIN'),
-            
-            ('RWB_DIEVDIRBIAI_DISTRICTS_FAITH_MODIFIER_GIVER_RELIGION_FOUNDED',     'ModifierId',        'RWB_DIEVDIRBIAI_RELIGION_FAITH_GAIN'),
+VALUES      ('RWB_DIEVDIRBIAI_DISTRICTS_FAITH_MODIFIER_GIVER',                      'ModifierId',        'RWB_DIEVDIRBIAI_FAITH_BURSTS'),
 
-            ('RWB_DIEVDIRBIAI_GREATPROPHET_FAITH_GAIN',                             'Amount',            '60'),
-            ('RWB_DIEVDIRBIAI_GREATPROPHET_FAITH_GAIN',                             'Scale',             '1'),
-            ('RWB_DIEVDIRBIAI_GREATPROPHET_FAITH_GAIN',                             'YieldType',         'YIELD_FAITH'),
-            
-            ('RWB_DIEVDIRBIAI_RELIGION_FAITH_GAIN',                             'Amount',            '30'),
-            ('RWB_DIEVDIRBIAI_RELIGION_FAITH_GAIN',                             'Scale',             '1'),
-            ('RWB_DIEVDIRBIAI_RELIGION_FAITH_GAIN',                             'YieldType',         'YIELD_FAITH'),
+            ('RWB_DIEVDIRBIAI_FAITH_BURSTS',                             'Amount',            '60'),
+            ('RWB_DIEVDIRBIAI_FAITH_BURSTS',                             'Scale',             '1'),
+            ('RWB_DIEVDIRBIAI_FAITH_BURSTS',                             'YieldType',         'YIELD_FAITH'),
             
             ('RWB_DIEVDIRBIAI_FAITH_GENERATION_MALUS',                              'YieldType',         'YIELD_FAITH'),
-            ('RWB_DIEVDIRBIAI_FAITH_GENERATION_MALUS',                              'Amount',            '-50'),
-            
-            ('RWB_DIEVDIRBIAI_GIVE_GOVERNOR_TITLE',                                 'Delta',             '1');
+            ('RWB_DIEVDIRBIAI_FAITH_GENERATION_MALUS',                              'Amount',            '-50');
+
 
 INSERT OR REPLACE INTO RequirementSets
             (RequirementSetId,                                                                          RequirementSetType) 
-VALUES      ('REQSET_RWB_DIEVDIRBIAI_GREATPROPHET_EXISTS_AND_IS_LITHUANIA',                             'REQUIREMENTSET_TEST_ALL'),
-            ('REQSET_RWB_DIEVDIRBIAI_HAS_BREATHTAKING_TILE',                                            'REQUIREMENTSET_TEST_ALL'),
-            ('REQSET_RWB_DIEVDIRBIAI_GREATPROPHET_EXISTS',                                              'REQUIREMENTSET_TEST_ALL'),
-            ('REQSET_RWB_DIEVDIRBIAI_FOUNDED_RELIGION_ONCE',                                            'REQUIREMENTSET_TEST_ALL'),
-            ('REQSET_RWB_DIEVDIRBIAI_FOUNDED_RELIGION_ONCE_AND_IS_LITHUANIA',                           'REQUIREMENTSET_TEST_ALL');
-
+VALUES      ('REQSET_RWB_DIEVDIRBIAI_HAS_BREATHTAKING_TILE',                                            'REQUIREMENTSET_TEST_ALL');
 
 
 INSERT OR REPLACE INTO RequirementSetRequirements
             (RequirementSetId,                                                                              RequirementId) 
-VALUES      ('REQSET_RWB_DIEVDIRBIAI_GREATPROPHET_EXISTS_AND_IS_LITHUANIA',         'RWB_DIEVDIRBIAI_REQUIRES_PLAYER_HAS_GREATPROPHET'),
-            ('REQSET_RWB_DIEVDIRBIAI_GREATPROPHET_EXISTS_AND_IS_LITHUANIA',         'RWB_DIEVDIRBIAI_REQUIRES_RELIGION_NOT_FOUNDED'),
-            ('REQSET_RWB_DIEVDIRBIAI_GREATPROPHET_EXISTS_AND_IS_LITHUANIA',         'RWB_DIEVDIRBIAI_REQUIRES_IS_LITHUANIAN_RWB_LEADER_VYTAUTAS'),
-
-            ('REQSET_RWB_DIEVDIRBIAI_GREATPROPHET_EXISTS',                          'RWB_DIEVDIRBIAI_REQUIRES_PLAYER_HAS_GREATPROPHET'),
-            ('REQSET_RWB_DIEVDIRBIAI_GREATPROPHET_EXISTS',                          'RWB_DIEVDIRBIAI_REQUIRES_RELIGION_NOT_FOUNDED'),
-            
-            ('REQSET_RWB_DIEVDIRBIAI_HAS_BREATHTAKING_TILE',                        'RWB_DIEVDIRBIAI_REQUIRES_APPEAL_4'),
-			
-			('REQSET_RWB_DIEVDIRBIAI_FOUNDED_RELIGION_ONCE',       'RWB_DIEVDIRBIAI_REQUIRES_RELIGION_FOUNDED'),
-			
-			('REQSET_RWB_DIEVDIRBIAI_FOUNDED_RELIGION_ONCE_AND_IS_LITHUANIA',       'RWB_DIEVDIRBIAI_REQUIRES_RELIGION_FOUNDED'),
-			('REQSET_RWB_DIEVDIRBIAI_FOUNDED_RELIGION_ONCE_AND_IS_LITHUANIA',       'RWB_DIEVDIRBIAI_REQUIRES_IS_LITHUANIAN_RWB_LEADER_VYTAUTAS');
+VALUES      ('REQSET_RWB_DIEVDIRBIAI_HAS_BREATHTAKING_TILE',                        'RWB_DIEVDIRBIAI_REQUIRES_APPEAL_4');
 
 
 INSERT OR REPLACE INTO Requirements
             (RequirementId,                                                         RequirementType,                                         Inverse) 
-VALUES      ('RWB_DIEVDIRBIAI_REQUIRES_PLAYER_HAS_GREATPROPHET',                    'REQUIREMENT_PLAYER_HAS_GREAT_PERSON_CLASS',  0),
-            ('RWB_DIEVDIRBIAI_REQUIRES_RELIGION_NOT_FOUNDED',                       'REQUIREMENT_PLAYER_FOUNDED_NO_RELIGION',                0),
-            ('RWB_DIEVDIRBIAI_REQUIRES_RELIGION_FOUNDED',                           'REQUIREMENT_PLAYER_FOUNDED_NO_RELIGION',                1),
-            ('RWB_DIEVDIRBIAI_REQUIRES_IS_LITHUANIAN_RWB_LEADER_VYTAUTAS',          'REQUIREMENT_PLAYER_TYPE_MATCHES',                0),
-            ('RWB_DIEVDIRBIAI_REQUIRES_APPEAL_4',                                   'REQUIREMENT_PLOT_IS_APPEAL_BETWEEN',                0);
+VALUES      ('RWB_DIEVDIRBIAI_REQUIRES_APPEAL_4',                                   'REQUIREMENT_PLOT_IS_APPEAL_BETWEEN',                0);
 
 
 INSERT OR REPLACE INTO RequirementArguments
             (RequirementId,                                                 Name,                       Value) 
-VALUES      ('RWB_DIEVDIRBIAI_REQUIRES_PLAYER_HAS_GREATPROPHET',            'GreatPersonClass',         'GREAT_PERSON_CLASS_PROPHET'),
-            ('RWB_DIEVDIRBIAI_REQUIRES_IS_LITHUANIAN_RWB_LEADER_VYTAUTAS',  'CivilizationType',         'CIVILIZATION_RWB_LITHUANIA'),
-            ('RWB_DIEVDIRBIAI_REQUIRES_APPEAL_4',                           'MinimumAppeal',            '4');
+VALUES      ('RWB_DIEVDIRBIAI_REQUIRES_APPEAL_4',                           'MinimumAppeal',            '4');
+
+-----------------------------------------------
+-- Owned Floodplains do not alter Appeal on adjacent tiles, and Reefs and Marshes instead generate +1 Appeal.
+-----------------------------------------------
+
+CREATE TABLE IF NOT EXISTS Rwb_FaithBurstFeatures
+(
+    FeatureType TEXT,
+    BurstValue INT
+);
+
+INSERT OR REPLACE INTO Rwb_FaithBurstFeatures
+        (FeatureType,       BurstValue)
+VALUES ('FEATURE_REEF',     1),('FEATURE_MARSH',    2) UNION
+SELECT	    Features.FeatureType,1 FROM Features WHERE FeatureType LIKE '%FLOODPLAIN%';                                           
 
 
---- Leadertype LEADER_RWB_VYTAUTAS
+-- 
 
---- REQUIREMENT_PLAYER_LEADER_TYPE_MATCHES
-            --('RWB_DIEVDIRBIAI_REQUIRES_HAS_BELIEF_',          'CITY_FOLLOWS_RELIGION_REQUIREMENTS',                0);
-			
-            /*('RWB_DIEVDIRBIAI_DISTRICTS_FAITH_SCALING_MODIFIER_GIVER',              'ModifierId',        'RWB_DIEVDIRBIAI_GREATPROPHET_FAITH_GAIN'),*/
-                                                                                                                                             
-            /*('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_DISTRICTS_GROWTH_MODIFIER_GIVER'),
-			('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_FAITH_PURCHASE_MALUS_BASE'     ),
-            ('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_APPEAL_BONUS_RELIGION'),
-            ('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_FAITH_GENERATION_MALUS_RELIGION'),
-            ('TRAIT_CIVILIZATION_RWB_DIEVDIRBIAI',          'RWB_DIEVDIRBIAI_FAITH_PURCHASE_MALUS_RWB_LEADER_VYTAUTAS_MODIFIER')*/
-			
-			
-			
-			                                                                              
-/*INSERT OR REPLACE INTO BeliefModifiers
-            (BeliefType, ModifierID)
+INSERT OR REPLACE INTO Types
+            (Type,					                                 Kind)
+SELECT	    'MODIFIER_RWB_DIEVDIRBIAI_APPEAL_ON_'||FeatureType,'KIND_MODIFIER'
+FROM Rwb_FaithBurstFeatures;
 
-SELECT      BeliefType,'RWB_DIEVDIRBIAI_APPEAL_MALUS_RWB_LEADER_VYTAUTAS'
-FROM Beliefs WHERE BeliefClassType = 'BELIEF_CLASS_FOLLOWER';*/
+--
 
-/*INSERT OR REPLACE INTO BeliefModifiers
-(BeliefType, ModifierID)
-SELECT      a.BeliefType,'RWB_DIEVDIRBIAI_FAITH_MALUS_'||b.LeaderType
-FROM Beliefs a, LeaderTraits b WHERE a.BeliefClassType = 'BELIEF_CLASS_FOLLOWER'
-                                 AND b.LeaderType IN (SELECT LeaderType FROM CivilizationLeaders WHERE CivilizationType IS 'CIVILIZATION_RWB_LITHUANIA') UNION
-
-SELECT      a.BeliefType,'RWB_DIEVDIRBIAI_GOLD_MALUS_'||b.LeaderType
-FROM Beliefs a, LeaderTraits b WHERE a.BeliefClassType = 'BELIEF_CLASS_FOLLOWER'
-                                 AND b.LeaderType IN (SELECT LeaderType FROM CivilizationLeaders WHERE CivilizationType IS 'CIVILIZATION_RWB_LITHUANIA');*/
+INSERT OR REPLACE INTO	TraitModifiers
+            (TraitType,									 ModifierId								)
+SELECT      'TRAIT_LEADER_RWB_UNION_OF_HORODLO','MODIFIER_RWB_DIEVDIRBIAI_APPEAL_ON_'||FeatureType
+FROM Rwb_FaithBurstFeatures;
 
 
+INSERT OR REPLACE INTO Modifiers
+            (ModifierId,                                                   ModifierType)
+SELECT      'MODIFIER_RWB_DIEVDIRBIAI_APPEAL_ON_'||FeatureType,  'MODIFIER_PLAYER_CITIES_ADJUST_FEATURE_APPEAL_MODIFIER'
+FROM Rwb_FaithBurstFeatures;
 
-/*('RWB_DIEVDIRBIAI_FAITH_GENERATION_MALUS_RELIGION',
-             'MODIFIER_PLAYER_CITIES_ADJUST_CITY_YIELD_MODIFIER',
-             'REQSET_RWB_DIEVDIRBIAI_FOUNDED_RELIGION_ONCE',
-             0),*/
 
---- REQSET_RWB_DIEVDIRBIAI_MALUS_PER_HABITANT
+INSERT OR REPLACE INTO ModifierArguments
+            (ModifierId,                                                   Name,                       Value)
+SELECT      'MODIFIER_RWB_DIEVDIRBIAI_APPEAL_ON_'||FeatureType,   'FeatureType',        FeatureType      FROM Rwb_FaithBurstFeatures     UNION
+SELECT      'MODIFIER_RWB_DIEVDIRBIAI_APPEAL_ON_'||FeatureType,   'Amount',             BurstValue       FROM Rwb_FaithBurstFeatures;
 
----MODIFIER_PLAYER_CITIES_ADJUST_CITY_YIELD_PER_POPULATION
---- MODIFIER_SINGLE_CITY_ADJUST_CITY_YIELD_PER_POPULATION 
---- CITY_FOLLOWS_RELIGION_REQUIREMENTS
+
+DROP TABLE Rwb_FaithBurstFeatures;
+
+-- MODIFIER_PLAYER_CITIES_ADJUST_FEATURE_APPEAL_MODIFIER
